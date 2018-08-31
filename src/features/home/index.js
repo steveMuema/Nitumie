2d4a42a1fbe1da9155a1d9b7d792a34d624bced2
hitchs3x <hitchs3x@gmail.com>
2018-08-31 16:45:53 +0300

    Update landing page to allow Register modal

--- src/features/home/index.js
+++ src/features/home/index.js
@@ -6,3 +6,4 @@ export { default as AppHeader } from './AppHeader';
 export { default as LandingPage } from './LandingPage';
 export { default as BasicDescription } from './BasicDescription';
 export { default as RegisterModal } from './RegisterModal';
+export { default as Register } from './Register';
