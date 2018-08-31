4560e1d0509b93d3e84daee85409f443107b86ee
hitchs3x <hitchs3x@gmail.com>
2018-08-31 17:09:27 +0300

    remove unwanted component

--- src/features/home/index.js
+++ src/features/home/index.js
@@ -1,5 +1,4 @@
 export { default as App } from './App';
-export { default as RedditList } from './RedditList';
 export { default as DefaultPage } from './DefaultPage';
 export { default as TestPage } from './TestPage';
 export { default as AppHeader } from './AppHeader';
